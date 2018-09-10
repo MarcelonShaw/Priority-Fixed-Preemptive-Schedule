2d90133908630bc4bee86e5a5f4d594d9dcf99dc
marcelojoss <marcelojoss99@gmail.com>
2018-09-10 18:17:34 -0300

    Add files via upload

---
+++ Prioridade.java
@@ -0,0 +1,22 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Prioridade {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n, entrada, tempoAtual, execucao, idProcessoAtual, qteProcessos;
+		ArrayList entrada_1, burst, prioridades, processos, cpEntrada;
+		int[] temposFinais, temposIniciais;
+		String ordemExecucao;
+		int contTeste = 0;
+		String formato, saida;
+		DecimalFormat decimal = new DecimalFormat("0.00");
+
+		System.out.println("quantos processadores deseja armazenar?");
+		n = scanner.nextInt();
+
+	}
+
+}
