34cff05f2618c0e52c3a9a61260ed383f6da26af
berlan2310 <kakachi1023@hotmail.com>
2018-09-10 18:38:02 -0300

    Update Prioridade.java

--- Prioridade.java
+++ Prioridade.java
@@ -5,33 +5,33 @@ import java.util.Scanner;
 public class Prioridade {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int n, entrada, tempoAtual, execucao, idProcessoAtual, qteProcessos;
-		ArrayList entrada_1, burst, prioridades, processos, cpEntrada;
-		int[] temposFinais, temposIniciais;
-		String ordemExecucao;
-		int contTeste = 0;
-		String formato, saida;
-		DecimalFormat decimal = new DecimalFormat("0.00");
+	Scanner scanner = new Scanner(System.in);
+	int n, entrada, tempoAtual, execucao, idProcessoAtual, qteProcessos;
+	ArrayList entrada_1, burst, prioridades, processos, cpEntrada;
+	int[] temposFinais, temposIniciais;
+	String ordemExecucao;
+	int contTeste = 0;
+	String formato, saida;
+	DecimalFormat decimal = new DecimalFormat("0.00");
 
-		System.out.println("quantos processadores deseja armazenar?");
-		n = scanner.nextInt();
+	System.out.println("quantos processadores deseja armazenar?");
+	n = scanner.nextInt();
                
-		while(n!= 0) {
-			contTeste++;
-                        processos = new ArrayList();
-                        entrada_1 = new ArrayList();
-                        burst = new ArrayList();
-                        prioridades = new ArrayList();
+	while(n!= 0) {
+        contTeste++;
+        processos = new ArrayList();
+        entrada_1 = new ArrayList();
+        burst = new ArrayList();
+        prioridades = new ArrayList();
 			
-                for(int i = 0; i < n; i++ ) {
-			System.out.println("Entrada do processo p" + (i + 1));
-                        entrada = scanner.nextInt();
-                        entrada_1.add(entrada);
-                        System.out.println("Qual o burst de p" + (i +1));
-                        entrada = scanner.nextInt();
-                        burst.add(entrada);
-                        System.out.println("Qual a prioridade de p" + (i +1));
+        for(int i = 0; i < n; i++ ) {
+	System.out.println("Entrada do processo p" + (i + 1));
+        entrada = scanner.nextInt();
+        entrada_1.add(entrada);
+        System.out.println("Qual o burst de p" + (i +1));
+        entrada = scanner.nextInt();
+        burst.add(entrada);
+        System.out.println("Qual a prioridade de p" + (i +1));
                         entrada =scanner.nextInt();
                         prioridades.add(entrada);
 			
